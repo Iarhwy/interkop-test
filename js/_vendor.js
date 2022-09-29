ad89a5d255c6213033986352587af84d19bf4a1f
Yaroslav <109588856+Iarhwy@users.noreply.github.com>
2022-09-29 17:33:12 +0300

    Delete js directory

--- js/_vendor.js
+++
@@ -1,9 +0,0 @@
-import './vendor/ymap.js';
-import './vendor/inputmask.min.js';
-import './vendor/just-validate3.3.3.min.js';
-import './vendor/burger.js';
-import './vendor/scroll.js';
-import './vendor/validation.js';
-// import './vendor/inputmask.js';
-import './vendor/goods-tabs.js';
-import './vendor/show-more.js';
--- js/app.js
+++
@@ -1,7 +0,0 @@
-import * as flsFunctions from "./modules/functions.js";
-
-flsFunctions.isWebp();
-
-import './_vendor.js';
-import vars from './_vars.js';
-// import './_components.js';
