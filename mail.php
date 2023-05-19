13374a4e2c3afff5e1caf32a1d9f1282e9f47d66
Yaroslav <109588856+Iarhwy@users.noreply.github.com>
2023-05-19 23:12:05 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,66 +4,76 @@ require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 require 'PHPMailer/Exception.php';
 
-$title = "Тема письма";
-$file = $_FILES['file'];
+# проверка, что ошибки нет
+if (!error_get_last()) {
 
-$c = true;
-// Формирование самого письма
-$title = "Заголовок письма";
-foreach ( $_POST as $key => $value ) {
-  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-    $body .= "
-    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-    </tr>
+    // Переменные, которые отправляет пользователь
+    $name = $_POST['name'] ;
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+    $file = $_FILES['myfile'];
+    
+    
+    // Формирование самого письма
+    $title = "Заголовок письма";
+    $body = "
+    <h2>Новое письмо</h2>
+    <b>Имя:</b> $name<br>
+    <b>Почта:</b> $email<br><br>
+    <b>Сообщение:</b><br>$text
     ";
-  }
-}
-
-$body = "<table style='width: 100%;'>$body</table>";
-
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-
-try {
-  $mail->isSMTP();
-  $mail->CharSet = "UTF-8";
-  $mail->SMTPAuth   = true;
-
-  // Настройки вашей почты
-  $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
-  $mail->Username   = 'mr-kiss'; // Логин на почте
-  $mail->Password   = 'qg7urwQ14NBnJzgmf2gE'; // Пароль на почте
-  $mail->SMTPSecure = 'ssl';//tls
-  $mail->Port       = 465; //587
-
-  $mail->setFrom('mr-kiss@mail.ru', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
-
-  // Получатель письма
-  $mail->addAddress('mr-kiss@mail.ru');
-
-  // Прикрипление файлов к письму
-  if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-      $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-      $filename = $file['name'][$ct];
-      if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-          $mail->addAttachment($uploadfile, $filename);
-          $rfile[] = "Файл $filename прикреплён";
-      } else {
-          $rfile[] = "Не удалось прикрепить файл $filename";
-      }
+    
+    // Настройки PHPMailer
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['data']['debug'][] = $str;};
+    
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'mr-kiss'; // Логин на почте
+    $mail->Password   = 'qg7urwQ14NBnJzgmf2gE'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';//tls
+    $mail->Port       = 465; //587
+    $mail->setFrom('mr-kiss@mail.ru', 'Заявка с сайта "Интеркоп"'); // Адрес самой почты и имя отправителя
+    
+    // Получатель письма
+    $mail->addAddress('yarffen@mail.ru');  
+    $mail->addAddress('interkop91@mail.ru'); // Ещё один, если нужен
+    
+    // Прикрипление файлов к письму
+    if (!empty($file['name'][0])) {
+        for ($i = 0; $i < count($file['tmp_name']); $i++) {
+            if ($file['error'][$i] === 0) 
+                $mail->addAttachment($file['tmp_name'][$i], $file['name'][$i]);
+        }
     }
-  }
-
-  // Отправка сообщения
-  $mail->isHTML(true);
-  $mail->Subject = $title;
-  $mail->Body = $body;
-
-  $mail->send();
-
-} catch (Exception $e) {
-  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+    
+    // Проверяем отправленность сообщения
+    if ($mail->send()) {
+        $data['result'] = "success";
+        $data['info'] = "Сообщение успешно отправлено!";
+    } else {
+        $data['result'] = "error";
+        $data['info'] = "Сообщение не было отправлено. Ошибка при отправке письма";
+        $data['desc'] = "Причина ошибки: {$mail->ErrorInfo}";
+    }
+    
+} else {
+    $data['result'] = "error";
+    $data['info'] = "В коде присутствует ошибка";
+    $data['desc'] = error_get_last();
 }
+
+// Отправка результата
+header('Content-Type: application/json');
+echo json_encode($data);
+
+?>
